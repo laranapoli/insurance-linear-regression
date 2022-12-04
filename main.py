c12306d50e38738f6f715bc6655f8136f936e8de
Lara Nápoli <laranapoli0@gmail.com>
2022-12-04 11:44:34 -0300

    improved app

--- main.py
+++ main.py
@@ -12,98 +12,113 @@ def get_model():
     
     return regressor
 
-with header:
-    st.title('Optimizing Health Insurance Pricing with Linear Regression')
-    st.caption(
-        'Insurance companies can benefit from customer information in business decision making, such as pricing its service.\
-        Having historical data to extract profile information on charges behavior might be useful for that purpose.'
-    )
+def main():
+    with header:
+        st.title('Optimizing Health Insurance Pricing with Linear Regression')
+        st.markdown(
+            """
+            Insurance companies can benefit from customer information in business decision makings, such as pricing its service. 
+            Having historical data to extract profile information on charges behavior might be useful for that purpose.
+            """
+        )
+        st.markdown("""<img
+                        src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2022/04/how_much_is_health_insurance.jpg"
+                        alt="Health Insurance Cost"
+                        height="320"
+                        width="660"
+                        />""", 
+                    unsafe_allow_html=True)
+            
 
-
-
-with dataset:
-    st.header('About the Data')
-    st.caption(
-        'The chosen dataset contains medical costs of people characterized by certain attributes and is available at \
-        [GitHub](https://github.com/stedy/Machine-Learning-with-R-datasets/blob/master/insurance.csv).' 
-    )
-    st.subheader('Attribute Information')
-    st.markdown(
-    """
-    - **Age**: Age of primary beneficiary.
-    - **Sex**: Insurance contractor gender (female, male).
-    - **BMI**: Body mass index, providing an understanding of body (weights that are relatively high or low relative to height).
-    - **Children**: Number of children covered by health insurance / Number of dependents.
-    - **Smoker**: Indicates whether the beneficiary is a smoker.
-    - **Region**: The beneficiary's residential area in the US, northeast, southeast, southwest, northwest.
-    - **Charges**: Individual medical costs billed by health insurance.
-    """
-    )
-
-
-with predictions:
-    st.header('Predicting Insurance Costs')
-
-    input_col, result_col = st.columns(2)
-
-    # input section
-    input_col.markdown(
+    with dataset:
+        st.header('About the Data')
+        st.caption(
+            'The chosen dataset contains medical costs of people characterized by certain attributes and is available at \
+            [GitHub](https://github.com/stedy/Machine-Learning-with-R-datasets/blob/master/insurance.csv).' 
+        )
+        st.subheader('Attribute Information')
+        st.markdown(
         """
-        ### Insured Information
+        - **Age**: Age of primary beneficiary.
+        - **Sex**: Insurance contractor gender (female, male).
+        - **BMI**: Body mass index, providing an understanding of body (weights that are relatively high or low relative to height).
+        - **Children**: Number of children covered by health insurance / Number of dependents.
+        - **Smoker**: Indicates whether the beneficiary is a smoker.
+        - **Region**: The beneficiary's residential area in the US, northeast, southeast, southwest, northwest.
+        - **Charges**: Individual medical costs billed by health insurance.
         """
-    )
-    input_col.caption(
-        'Please provide the information below to estimate the cost of insurance.'
-    )
-
-    children = input_col.slider(
-        "How many dependents does the beneficiary have?",
-        min_value=0,
-        max_value=5,
-        value=0,
-        step=1
-    )
-
-    age = input_col.number_input(
-        "What is the beneficiary's age?",
-        min_value=18,
-        max_value=65,
-        value=41,
-        step=1
-    )
-
-    bmi = input_col.number_input(
-        "Beneficiary's body mass index",
-        min_value=15.0,
-        max_value=55.0,
-        value=35.0,
-        step=0.05
-    )
+        )
 
 
-    smoker = input_col.selectbox(
-        "Is the beneficiary a smoker?",
-        options=['yes', 'no']
-    )
+    with predictions:
+        st.header('Predicting Insurance Costs')
 
-    # output section
+        input_col, result_col = st.columns(2)
 
-    result_col.markdown(
-        """
-        ### Predicted Values
-        """
-    )
+        # input section
+        input_col.markdown(
+            """
+            ### Insured Information
+            """
+        )
+        input_col.caption(
+            'Please provide the information below to estimate the cost of insurance.'
+        )
 
-    df = pd.DataFrame({
-        'age': [age],
-        'bmi': [bmi],
-        'children': [children],
-        'smoker': [smoker]
-    })
+        children = input_col.slider(
+            "How many dependents does the beneficiary have?",
+            min_value=0,
+            max_value=5,
+            value=0,
+            step=1
+        )
 
-    regressor = get_model()
-    charges = round(regressor.predict(df)[0],2)
+        age = input_col.number_input(
+            "What is the beneficiary's age?",
+            min_value=18,
+            max_value=65,
+            value=41,
+            step=1
+        )
 
-    result_col.caption("From the information provided:")
+        bmi = input_col.number_input(
+            "Beneficiary's body mass index",
+            min_value=15.0,
+            max_value=55.0,
+            value=35.0,
+            step=0.05
+        )
 
-    result_col.write(f"The likely amount of charges associated with this profile is U$ {charges}.")
+
+        smoker = input_col.selectbox(
+            "Is the beneficiary a smoker?",
+            options=['yes', 'no']
+        )
+
+        pred_button = input_col.button("Estimate Cost")
+
+        # output section
+        result_col.markdown(
+            """
+            ### Predicted Values
+            """
+        )
+
+        result_col.caption("From the information provided:")
+
+        if pred_button:
+            df = pd.DataFrame({
+            'age': [age],
+            'bmi': [bmi],
+            'children': [children],
+            'smoker': [smoker]
+            })
+
+            regressor = get_model()
+            charges = round(regressor.predict(df)[0],2)
+            
+            result_col.info(f"The likely amount of charges associated with this profile is U$ {charges}.")
+       
+
+if __name__ == '__main__':
+	main()
